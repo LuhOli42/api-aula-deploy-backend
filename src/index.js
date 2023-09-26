1852a319fd4f780d742fdf331ffc4388c6213b2a
Ideraldo Silva <luh.oli.42@gmail.com>
2023-09-26 17:40:02 -0300

    pq n funcionas?

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
-const porta = process.env.PORT;
 
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  return res.json("api funfando");
+  return res.json("Api está Ok!");
 });
 
-app.listen(porta, () => {
-  console.log(`Servidor na porta:${process.env.PORT}`);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Servidor em pé na porta ${port}`);
 });
